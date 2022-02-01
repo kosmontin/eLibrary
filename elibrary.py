b428b84e21091c63cca9508d87b6e76f0aafe5bc
Konstantin Beloborodov <kosta.white@gmail.com>
2022-02-01 15:22:55 +0300

    Step 4 of 16: add check_for_redirect function

--- elibrary.py
+++ elibrary.py
@@ -3,6 +3,11 @@ import os
 import requests
 
 
+def check_for_redirect(response):
+    if response.history:
+        raise requests.exceptions.HTTPError
+
+
 def main():
     url = 'http://tululu.org/txt.php'
 
@@ -12,9 +17,12 @@ def main():
     for book_id in range(1, 11):
         response = requests.get(url, params={'id': book_id})
         response.raise_for_status()
-        if response.ok:
+        try:
+            check_for_redirect(response)
             with open(f'./books/id{book_id}.txt', 'wb') as file:
                 file.write(response.content)
+        except requests.exceptions.HTTPError:
+            print(f'Book #{book_id} not found')
 
 
 if __name__ == '__main__':
