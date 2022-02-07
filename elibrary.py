f1c45786b5e70cacb4a4927d1f308287233543d4
Konstantin Beloborodov <kosta.white@gmail.com>
2022-02-07 16:46:01 +0300

    Step 4 of 9: add parser for categories

--- elibrary.py
+++ elibrary.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 import os
 from urllib.parse import urljoin, urlparse
 
@@ -7,8 +8,11 @@ import requests
 from bs4 import BeautifulSoup
 from tqdm import tqdm
 
+from parse_tululu_category import get_books_id
+
 URL_BOOK_PAGE = 'http://tululu.org/b{}/'
 URL_BOOK_DOWNLOAD = 'http://tululu.org/txt.php'
+URL_FANTASY_CATEGORY = 'http://tululu.org/l55/'
 
 
 def check_for_redirect(response, text=''):
@@ -18,20 +22,24 @@ def check_for_redirect(response, text=''):
 
 def download_image(url, folder='images'):
     filename = os.path.basename(urlparse(url).path)
+    path = os.path.join(folder, filename)
     response = requests.get(url)
     response.raise_for_status()
     os.makedirs(folder, exist_ok=True)
-    with open(os.path.join(folder, filename), 'wb') as file:
+    with open(path, 'wb') as file:
         file.write(response.content)
+    return path
 
 
 def download_txt(book_id, filename, folder='books'):
+    path = os.path.join(folder, pathvalidate.sanitize_filename(filename))
     response = requests.get(URL_BOOK_DOWNLOAD, params={'id': book_id})
     response.raise_for_status()
     check_for_redirect(response, 'Trying to download txt file')
     os.makedirs(folder, exist_ok=True)
-    with open(os.path.join(folder, pathvalidate.sanitize_filename(filename)), 'w') as file:
+    with open(path, 'w', encoding='utf-8') as file:
         file.write(response.text)
+    return path
 
 
 def parse_book_page(page_content):
@@ -48,24 +56,38 @@ def parse_book_page(page_content):
 
 
 def main():
-    parse = argparse.ArgumentParser()
-    parse.add_argument('start_id', type=int, nargs='?', default=1)
-    parse.add_argument('end_id', type=int, nargs='?', default=0)
-    args = parse.parse_args()
+    # parse = argparse.ArgumentParser()
+    # parse.add_argument('start_id', type=int, nargs='?', default=1)
+    # parse.add_argument('end_id', type=int, nargs='?', default=0)
+    # args = parse.parse_args()
+    # for book_id in tqdm(range(args.start_id, args.start_id + 1 if not args.end_id else args.end_id + 1)):
+    books_info = []
 
-    for book_id in tqdm(range(args.start_id, args.start_id + 1 if not args.end_id else args.end_id + 1)):
+    for book_id in tqdm(get_books_id()):
         try:
             response = requests.get(URL_BOOK_PAGE.format(book_id))
             response.raise_for_status()
             check_for_redirect(response)
             page_soup = BeautifulSoup(response.text, 'lxml')
             book_info = parse_book_page(page_soup)
-            download_txt(book_id, f'{book_id}. {book_info["book_name"]}.txt')
-            download_image(book_info['book_image_url'])
+
+            books_info.append(
+                {
+                    'title': book_info['book_name'],
+                    'author': book_info['book_author'],
+                    'img_src': download_image(book_info['book_image_url']),
+                    'book_path': download_txt(book_id, f'{book_id}. {book_info["book_name"]}.txt'),
+                    'comments': book_info['book_comments'],
+                    'genres': book_info['book_genres']
+                }
+            )
         except (requests.HTTPError, requests.ConnectionError) as e:
             print(f'\n\nError downloading the book #{book_id}\n')
             print('Details below: ')
             print(e.args)
+
+    with open('books_info.json', 'w', encoding='utf-8') as json_file:
+        json.dump(books_info, json_file, ensure_ascii=False)
 
 
 if __name__ == '__main__':
---
+++ parse_tululu_category.py
@@ -0,0 +1,23 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL_FANTASY_CATEGORY = 'http://tululu.org/l55/'
+URL_SITE = 'http://tululu.org'
+
+
+def get_books_id(url=URL_FANTASY_CATEGORY):
+    books_id = []
+    num_page = 1
+    while num_page <= 4:
+        response = requests.get(f'{url}{num_page}/')
+        response.raise_for_status()
+        soup = BeautifulSoup(response.text, 'lxml')
+        books_soup = soup.find('div', {'id': 'content'}).find_all('table', class_='d_book')
+        for book in books_soup:
+            books_id.append(book.find('tr', {'align': ''}).find('a')['href'][2:-1])
+        num_page += 1
+    return books_id
+
+
+if __name__ == '__main__':
+    pass
